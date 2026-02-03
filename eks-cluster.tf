1dd9be66d0dd3ab40bb3401ad9571f9f92fcfd90
yassine100000000 <hamdisyassine57@gmail.com>
2026-02-03 20:22:58 +0100

    updating eks-cluster

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -6,11 +6,14 @@ module "eks" {
   kubernetes_version = "1.33"
   endpoint_public_access  = true
 
+# Most Important Line : Pods will run in private network. - To access internet, they need NAT. - To expose app, you’ll need Load Balancer or Ingress.
   subnet_ids = module.myapp-vpc.private_subnets
   vpc_id = module.myapp-vpc.vpc_id
 
+  # Whoever created this cluster is admin inside Kubernetes
   enable_cluster_creator_admin_permissions = true  
 
+# These are default Kubernetes components AWS installs
 #   addons = {
 #     coredns                = {}
 #     eks-pod-identity-agent = {
@@ -22,11 +25,11 @@ module "eks" {
 #     }
 #   }
   
-  tags = {
+  tags = 
     environment = "development"
     application = "myapp"
-  }
 
+# This is the node group configuration. We are using AWS managed node groups, which means that AWS will take care of provisioning and managing the EC2 instances that will run our Kubernetes worker nodes.
   eks_managed_node_groups = {
     dev = {
     instance_types = ["t2.small"]
